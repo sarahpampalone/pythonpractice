41e2f5d8132cb7823c7c9fad24248487f2ba6fc5
sarahpampalone <120332370+sarahpampalone@users.noreply.github.com>
2022-12-29 13:11:50 -0500

    Create reversewordorder.py

---
+++ reversewordorder.py
@@ -0,0 +1,19 @@
+#Write a program (using functions!) that asks the user for a
+# long string containing multiple words. Print back to the user
+# the same string, except with the words in backwards order. For
+# example, say I type the string:
+#   My name is Michele
+#Then I would see the string:
+#   Michele is name My
+#shown back to me.
+ 
+def split(s) :
+    return s.split()
+ 
+def flip(s) :
+    return s.reverse()
+ 
+phrase = input("Please enter a long string: ")
+temp = split(phrase)
+flip(temp)
+print(temp)
